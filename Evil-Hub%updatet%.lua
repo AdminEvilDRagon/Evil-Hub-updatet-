36cfc10baadec73049e7640519a0ae70c09429ac
AdminEvilDRagon <92892215+AdminEvilDRagon@users.noreply.github.com>
2022-08-03 10:20:36 +0200

    Create Evil-Hub%updatet%.lua

---
+++ Evil-Hub%updatet%.lua
@@ -0,0 +1,38 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+
+local Window = Library.CreateLib("Evil Hub            Made by Evil Dragon#0001", "Sentinel")
+local Tab = Window:NewTab("Mad City")
+local MadCitySection = Tab:NewSection("Mad City")
+local Tab = Window:NewTab("Crazy Pet Simulator")
+local CrazyPetSimulatorSection =  Tab:NewSection("Crazy Pet Simulator")
+local Tab = Window:NewTab("Batting Champions!")
+local BattingChampionsSection =  Tab:NewSection("Batting Champions!")
+local Tab = Window:NewTab("Fight in Train station")
+local Fight in Train stationSection("Fight in Train station")
+
+
+
+
+
+MadCitySection:NewButton("Mad City", "Mad City", function(v)
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/AdminEvilDRagon/Madcity/main/MadCity.lua", true))()
+end)
+
+
+CrazyPetSimulatorSection:NewButton("Crazy Pet Simulator", "Crazy Pet Simulator", function(v)
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/AdminEvilDRagon/crazy-pet-obfuse/main/crazy%20pet%20obfuse.lua", true))()
+end)
+
+
+BattingChampionsSection:NewButton("Batting Champions", "Batting Champions", function(v)
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/AdminEvilDRagon/batting-Champions/main/Champions.lua", true))()
+end)
+
+FightinTrainstationSection:NewButton("Fight in Train station", "Fight in Train station", function(v)
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/AdminEvilDRagon/fight/main/fight.lua", true))()
+end)
+
+
+
+
+
