1772d333f3a6ee874f2f5e4b2e1c0ee273d23d09
AdminEvilDRagon <92892215+AdminEvilDRagon@users.noreply.github.com>
2022-08-27 20:02:31 +0200

    Update Evil-Hub updatet 9.8.lua

--- Evil-Hub updatet 9.8.lua
+++ Evil-Hub updatet 9.8.lua
@@ -17,7 +17,7 @@ local DrinkingSimulatorSection = Tab:NewSection("Drinking Simulator")
 
 
 MadCitySection:NewButton("Mad City", "Mad City", function(v)
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/AdminEvilDRagon/New-Update/main/New%20update.lua", true))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/AdminEvilDRagon/New-Update/main/Update%20New.lua", true))()
 end)
 
 
